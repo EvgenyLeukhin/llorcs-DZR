c24bf0f792c24286797a67413483d8d88ec970a0
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2020-04-14 16:14:21 +0600

    Add animation on a start screen

--- src/javascripts/scenes/start-scene.js
+++ src/javascripts/scenes/start-scene.js
@@ -3,32 +3,32 @@ import { Linear, TimelineLite, TweenLite } from 'gsap';
 
 const makeStartScene = () => {
   const startSceneTimeline = new TimelineLite()
-    // анимация от и до продолжительностью 500
-    .add([
-      TweenLite.fromTo('#start-scene', 500, {
-        css: {
-          opacity: 0,
-          zIndex: 1,
-        },
-      }, {
-        ease: Linear.easeNone,
-        css: {
-          opacity: 1,
-          zIndex: 15,
-        },
-      }),
-    ])
-    // анимация точечная продолжительностью 500, задержкой 1000
+
+    // анимация на появление при загрузке длительностью 500
     .add([
       TweenLite.to('#start-scene', 500, {
-        css: {
-          display: 'none',
-          opacity: 0,
-          zIndex: 15,
-        },
-        // delay: 1000,
+        css: { display: 'block' },
+      }),
+    ])
+
+    // анимация на исчезновение продолжительностью 500 + параллельные анимации картинок 500
+    .add([
+      TweenLite.to('#start-scene', 500, {
+        css: { display: 'none' },
         ease: Linear.easeNone,
       }),
+
+      // animate cards
+      TweenLite.to('.start-scene-photo-1', 500, { y: 1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-2', 500, { y: -1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-3', 500, { x: 1000, y: -1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-4', 500, { x: 1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-5', 500, { x: -1000, y: -1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-6', 500, { x: -1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-7', 500, { x: 1000, y: 1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-8', 500, { x: -1000, y: 1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-9', 500, { x: 500, y: 1000, ease: Linear.easeNone }),
+      TweenLite.to('.start-scene-photo-10', 500, { x: -500, y: 1000, ease: Linear.easeNone }),
     ]);
 
   return new ScrollMagic.Scene({
