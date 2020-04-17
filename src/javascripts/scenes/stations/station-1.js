27e927831b74252b1e3da5f4d81f3d2c8ccc1a32
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2020-04-17 14:33:04 +0600

    Anime start

--- src/javascripts/scenes/stations/station-1.js
+++ src/javascripts/scenes/stations/station-1.js
@@ -5,7 +5,7 @@ const makeStation1Scene = () => {
   const stationSceneTimeline = new TimelineLite()
     .add([
       // show station
-      TweenLite.fromTo('#station-1', 3000, { // show station duration
+      TweenLite.fromTo('#station-1', 8000, { // show station duration
         // styles before
         x: '200%',
         display: 'none',
@@ -21,58 +21,104 @@ const makeStation1Scene = () => {
         ease: Linear.easeNone,
       }),
 
-      // show station scene
-      TweenLite.fromTo('.station-1-scene-photo-1', 6000,
-        { y: '-50vh', ease: Linear.easeNone },
-        { y: 0, ease: Linear.easeNone, delay: 1000 }),
-
-      TweenLite.fromTo('.station-1-scene-photo-2', 6000,
-        { y: 2000, ease: Linear.easeNone },
-        { y: 0, rotation: 5, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-3', 6000,
-        { y: -2000, ease: Linear.easeNone },
-        { y: 0, rotation: 8, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-4', 6000,
-        { y: -1000, ease: Linear.easeNone },
-        { y: 0, rotation: -5, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-5', 6000,
-        { y: 1000, ease: Linear.easeNone },
-        { y: 0, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-6', 6000,
-        { y: 2000, ease: Linear.easeNone },
-        { y: 0, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-7', 6000,
-        { y: -2000, ease: Linear.easeNone },
-        { y: 0, rotation: -7, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-8', 6000,
-        { y: 2000, ease: Linear.easeNone },
-        { y: 0, rotation: -3, ease: Linear.easeNone }),
-
-      TweenLite.fromTo('.station-1-scene-photo-9', 6000,
-        { y: -1000, ease: Linear.easeNone },
-        { y: 0, ease: Linear.easeNone }),
-
-      TweenLite.to('#station-1-scene', 6000, {
+      // 1-2 card show
+      TweenLite.fromTo('.station-1-scene-photo-1', 2000,
+        {
+          x: '-50vw',
+          y: '-100vh',
+          ease: Linear.easeNone,
+          rotation: -320,
+        },
+        {
+          x: 0,
+          y: 0,
+          ease: Linear.easeNone,
+          delay: 5000,
+          rotation: 0,
+        }),
+      TweenLite.fromTo('.station-1-scene-photo-2', 2000,
+        {
+          x: '50vw',
+          y: '100vh',
+          ease: Linear.easeNone,
+          rotation: -320,
+        },
+        {
+          x: 0,
+          y: 0,
+          rotation: 5,
+          ease: Linear.easeNone,
+          delay: 5000,
+        }),
+      // 1-2 card hide
+      TweenLite.to('.station-1-scene-photo-1', 2000, {
+        x: '-300vw',
+        display: 'none',
         ease: Linear.easeNone,
+        delay: 7000,
+      }),
+      TweenLite.to('.station-1-scene-photo-2', 2000, {
+        x: '-300vw',
+        display: 'none',
+        ease: Linear.easeNone,
+        delay: 7000,
+      }),
+
+      // 3-5 card show
+      TweenLite.fromTo('.station-1-scene-photo-3', 2000,
+        {
+          x: '100vw',
+          y: '-100vh',
+          ease: Linear.easeNone,
+          rotation: -320,
+        },
+        {
+          x: 0,
+          y: 0,
+          ease: Linear.easeNone,
+          delay: 6000,
+          rotation: 8,
+        }),
+      TweenLite.fromTo('.station-1-scene-photo-5', 2000,
+        {
+          x: '100vw',
+          y: '100vh',
+          ease: Linear.easeNone,
+          rotation: -320,
+        },
+        {
+          x: 0,
+          y: 0,
+          ease: Linear.easeNone,
+          delay: 6000,
+          rotation: 0,
+        }),
+
+      // 3-5 card hide
+      TweenLite.to('.station-1-scene-photo-3', 2000, {
+        x: '-300vw',
+        display: 'none',
+        ease: Linear.easeNone,
+        delay: 8000,
+      }),
+      TweenLite.to('.station-1-scene-photo-5', 2000, {
+        x: '-300vw',
+        display: 'none',
+        ease: Linear.easeNone,
+        delay: 8000,
       }),
     ])
 
     // hide station scene
     .add([
-      TweenLite.to('#station-1-scene', 1500, {
+      TweenLite.to('#station-1-scene', 1000, {
         x: '-300%',
         display: 'none',
         ease: Linear.easeNone,
       }),
 
       // hide station
-      TweenLite.to('#station-1', 3000, {
+      TweenLite.to('#station-1', 8000, {
         x: '-200%',
         display: 'none',
         ease: Linear.easeNone,
@@ -81,7 +127,7 @@ const makeStation1Scene = () => {
 
   return new ScrollMagic.Scene({
     triggerElement: '#station-1-trigger',
-    duration: '400%',
+    duration: '4000%',
     offset: -300,
   })
     .setTween(stationSceneTimeline);
